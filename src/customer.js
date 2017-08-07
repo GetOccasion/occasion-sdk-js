2c9637dc5981155034df021a05f35db0aa3218d1
Nick Landgrebe <nick@landgre.be>
2017-08-07 12:50:17 -0400

    Remove customer signIn

--- src/customer.js
+++ src/customer.js
@@ -1,9 +1,5 @@
 Occasion.Modules.push(function(library) {
-  library.Customer = class Customer extends library.Base {
-    signIn(email, password) {
-      this.resourceLibrary.interface.request(this.links()['related'] + 'sign_in', 'POST', { email: email, password: password });
-    }
-  };
+  library.Customer = class Customer extends library.Base {};
 
   library.Customer.className = 'Customer';
   library.Customer.queryName = 'customers';
