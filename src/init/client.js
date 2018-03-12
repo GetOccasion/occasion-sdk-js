7b0f1db448ec51f9703778cb759b905fdef2133f
Pelle ten Cate <pelle@occsn.com>
2018-03-12 15:19:33 -0500

    Add Atlas tracker loader

--- src/init/client.js
+++ src/init/client.js
@@ -5,6 +5,14 @@ class Occasion {
 
   static Client(options = {}) {
     var url = options.baseUrl || Occasion.baseUrl;
+    var host = url.match(/\w+:\/\/[^\/]+/);
+
+    var atlas = document.createElement("script");
+    atlas.type = "text/javascript";
+    atlas.async = true;
+    atlas.src = `${host}/p/tracker.js`;
+    document.getElementsByTagName('head')[0].appendChild(atlas);
+
     var token = options.token;
     var immutable = options.immutable || false;
 
